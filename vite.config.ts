bc2f7972d91ee03810855027839a40999d76c2fb
Erik <erik@Mac.lan>
2025-12-11 12:51:28 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/1998/',
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
