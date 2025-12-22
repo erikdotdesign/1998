4d9fa308e7a09c966a33c2b5aeb30e83b251432e
Erik <erik@Mac.lan>
2025-12-22 10:37:30 -0500

    added doom info modal

--- src/DoomWindow.tsx
+++ src/DoomWindow.tsx
@@ -1,10 +1,13 @@
 import WindowContainer from "./WindowContainer";
 import useWindowManager from './WindowManager';
+import InfoIcon from "./assets/images/icons/info.png";
 
 import "./DoomWindow.css";
+import { useState } from "react";
 
 const DoomWindow = () => {
   const { windows, dispatch } = useWindowManager();
+  const [showModal, setShowModal] = useState(true);
   
   const win = Object.values(windows).find(w => w.windowType === "doom");
 
@@ -24,6 +27,18 @@ const DoomWindow = () => {
           className="c-doom-window__iframe"
           src="./doom/index.html"
           allow="autoplay; fullscreen" />
+        {
+          showModal
+          ? <div className="c-doom-window__modal">
+              <img src={InfoIcon} />
+              <div>
+                <h5>Keyboard Required</h5>
+                <p><i>This game also has sound</i></p>
+              </div>
+              <button onClick={() => setShowModal(false)}>Ok</button>
+            </div>
+          : null
+        }
       </div>
     </WindowContainer>
   );
